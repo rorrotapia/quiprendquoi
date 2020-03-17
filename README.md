8993fa4ea176e0930f1171a7d1807b5f85b1733b
rorrotapia <rodrigo.tapia-bravo@hetic.net>
2020-03-17 19:31:16 +0100

    save

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# quiprendquoi
+# TP - Réalisation d'une application "high-low tech"
+
+L'application à réaliser est un "qui prend quoi" : un utilisateur créé un événément, le partage, et tout le monde peut ajouter les éléments qu'il compte amener pour cet événement (liste partagée). Chaque étudiant est libre d'adapter le cahier des charges de cette application de base.
+
+[Repository & documentation de l'API](https://github.com/bcalou/quiprendquoi-api)
+
+## TICKETS
+
+### Jour 1 - [This is for everyone](doc/01_this_is_for_everyone.md) 100%
+### Jour 2 - [Move fast, don't break things](doc/02_move_fast_dont_break_things.md) 0%
+### Jour 3 - All the right vitamins
+
+
+### Rodrigo :D
--- app.js
+++ app.js
@@ -12,12 +12,15 @@ app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 });
 
+app.get('/err', function(req, res) {
+    res.render('err', { title: '404' });
+});
+
 app.post('/party', function(req, res) {
     axios
         .post(`${process.env.API_URL}/party`, req.body)
-        .then(({data}) => console.log(data))
-        .catch((err) => console.error(err));
-    res.send('Post ok !')
+        .then(({ data }) => res.redirect(`/party/${data._id}`))
+        .catch((err) => res.redirect(`/err`));
 });
 
 app.get('/party/:id', function(req, res) {
@@ -26,11 +29,28 @@ app.get('/party/:id', function(req, res) {
         .then(({ data }) =>
             res.render('party', {
                 party: data,
-                title: data.name
+                title: data.name,
+                items: data.items,
+                url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`
             }),
         )
         .catch((err) => console.log(err));
 });
+
+app.post('/additem', function(req, res) {
+    axios
+        .post(`${process.env.API_URL}/party/${req.body.party}/items`, req.body)
+        .then(({ data }) => res.redirect(`/party/${req.body.party}`))
+        .catch((err) => res.redirect(`/err`));
+});
+
+app.post('/deleteitem', function(req, res) {
+    axios
+        .delete(`${process.env.API_URL}/party/${req.body.id_party}/items/${req.body.item}`, req.body)
+        .then(({ data }) => res.redirect(`/party/${req.body.id_party}`))
+        .catch((err) => res.redirect(`/err`));
+});
+
 
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
