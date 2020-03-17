48c5a3d56e5e209724dc21bc0c7c973f9f31cedd
rorrotapia <rodrigo.tapia-bravo@hetic.net>
2020-03-18 00:41:52 +0100

    save

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -13,25 +13,6 @@ if (navigator.clipboard) {
     console.warn("Pas de support")
 }
 
-if (navigator.share) {
-    document.querySelectorAll('[data-share-url]').forEach(($shareEl) => {
-        const $button = document.createElement('button');
-        $button.innerHTML = 'Partager';
-        $shareEl.parentNode.append($button);
-
-        $button.addEventListener('click', share.bind(this, $shareEl));
-    });
-}
-
-function share($shareEl) {
-    navigator.share({
-        title: $shareEl.getAttribute('data-share-title'),
-        text: $shareEl.getAttribute('data-share-text'),
-        url: $shareEl.getAttribute('data-share-url'),
-    });
-}
-
-
 function copyToClipboard($clipboardEl, $button) {
     navigator.clipboard
         .writeText($clipboardEl.getAttribute('data-clipboard'))
